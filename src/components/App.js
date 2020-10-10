9fb9381a8442d6a5c29e8ef1576749c02ef6dbb2
Alex Cevallos <acevallos@linkedin.com>
2020-10-10 09:54:15 -0700

    Sorting appointments going into the component by manipulating the state

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
       lastIndex: 0
     };
 
@@ -62,6 +64,23 @@ class App extends Component {
   }
 
   render() {
+
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() <  b[this.state.orderBy].toLowerCase()) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    });
+
     return (
       <main className="page bg-white" id="petratings">
       <div className="container">
@@ -70,7 +89,7 @@ class App extends Component {
             <div className="container">
               <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} addAppointment = {this.addAppointment}/>
               <SearchAppointments />
-              <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment} />
+              <ListAppointments appointments={filteredApts} deleteAppointment={this.deleteAppointment} />
             </div>
           </div>
         </div>
